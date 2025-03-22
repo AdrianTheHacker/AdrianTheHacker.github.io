9177dd6ffe8a52b0cd34adaa774ad93375c2f327
AdrianTheHacker <57376726+AdrianTheHacker@users.noreply.github.com>
2025-03-22 16:35:25 -0400

    Adding Meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,10 @@ export default function RootLayout({
     <html lang="en" data-theme='sunset'>
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta charSet="UTF-8" />
+        <meta name="description" content="First Year Electrical Engineering Student at The University of Waterloo" />
+        <meta name="keywords" content="Personal Website, Portfolio Website, Adrian Tarantino, AdrianTheHacker, Adrian The Hacker, NextJS, Javascript" />
+        <meta name="author" content="Adrian Tarantino" />
         <title>Adrian Tarantino - Personal Website</title>
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
