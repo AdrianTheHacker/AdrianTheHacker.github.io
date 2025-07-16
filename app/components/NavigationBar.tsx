6d3fa27e5e22a4406932548f1a557a26cfbd4c40
Adrian Tarantino <adriantarantino2006@gmail.com>
2025-07-16 07:32:29 -0400

    Creating temporary fix to mobilel viewing bug

--- app/components/NavigationBar.tsx
+++ app/components/NavigationBar.tsx
@@ -2,24 +2,25 @@ import React from 'react'
 
 const NavigationBar = () => {
   return (
-    <div className="navbar bg-base-100 shadow-sm z-50 absolute">
+    <div className="navbar bg-base-100 shadow-sm">
       <div className="flex-1">
-        <a className="btn btn-ghost text-xl">daisyUI</a>
+        <a className='btn btn-ghost text-xl' href='mailto:adriantarantino2006@gmail.com'>Email Me</a>
       </div>
       <div className="flex-none">
         <ul className="menu menu-horizontal px-1">
-          <li><a href='mailto:adriantarantino2006@gmail.com'>Email Me</a></li>
+          {/* <li><a href='mailto:adriantarantino2006@gmail.com'>Email Me</a></li> */}
           <li><a href='https://adrianthehacker.notion.site/Adrian-s-Blog-2326e2b064d4804097b1eea96204696c'>Blog</a></li>
           <li><a>Portfolio</a></li> 
           <li>
-            <details>
+            <a href='https://www.linkedin.com/in/adriantarantino/'>LinkedIn</a>
+            {/* <details>
               <summary>Socials</summary>
               <ul className="bg-base-100 rounded-t-none p-2">
                 <li><a href='https://www.linkedin.com/in/adriantarantino/'>LinkedIn</a></li>
                 <li><a href='https://github.com/AdrianTheHacker'>GitHub</a></li>
                 <li><a href='https://www.youtube.com/@AdrianTheHacker'>YouTube</a></li>
               </ul>
-            </details>
+            </details> */}
           </li>
         </ul>
       </div>
